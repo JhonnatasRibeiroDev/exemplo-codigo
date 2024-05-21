b1125df914168064a4c4c4e248c06108273b75d6
Jhonnatas Ribeiro <97709889+JhonnatasRibeiroDev@users.noreply.github.com>
2024-05-20 23:44:56 -0300

    Configurei a aplicação da maneira inicial com algumas estruturas basicas de pastas

---
+++ usandoDadosProprios/Enzinho-project/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+function App(){
+  return (
+    <div>
+      <h1>Enzinho</h1>
+      <p>Enzinho é um projeto de um site de notícias que tem como objetivo informar sobre os acontecimentos do mundo de forma rápida e prática.</p>
+    </div>
+  )
+}export default App;
