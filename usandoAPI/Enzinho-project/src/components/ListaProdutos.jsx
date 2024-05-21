29238db8292b157493fcfcdd78693a100fcf8ca5
Jhonnatas Ribeiro <97709889+JhonnatasRibeiroDev@users.noreply.github.com>
2024-05-21 02:07:06 -0300

    completo com uso dos cors

--- usandoAPI/Enzinho-project/src/components/ListaProdutos.jsx
+++ usandoAPI/Enzinho-project/src/components/ListaProdutos.jsx
@@ -1,23 +1,33 @@
-//import React from 'react';
+import { useEffect, useState } from 'react';
 import Produto from "./Produto";
-import produtos from "../data/produtos";
+import { obterProdutos } from "../data/produtos";
 import "../styles/style.listaProdutos.css";
 
 function ListaProdutos() {
-  return (
-    <div className="listaProdutos">
-      {produtos.length>0 ? (
-        produtos.map(produto => (
-            <Produto
-              key={produto.id}
-              produto={produto} // Passando o objeto produto como uma propriedade chamada 'produto'
-            />
-          ))
-      ): (
-        <div>Nenhum produto disponível</div>
-      )}
-    </div>
-  );
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {
+        async function carregarProdutos() {
+            const produtosDaApi = await obterProdutos();
+            setProdutos(produtosDaApi);
+        }
+        carregarProdutos();
+    }, []);
+
+    return (
+        <div className="listaProdutos">
+            {produtos.length > 0 ? (
+                produtos.map(produto => (
+                    <Produto
+                        key={produto.id}
+                        produto={produto} // Passando o objeto produto como uma propriedade chamada 'produto'
+                    />
+                ))
+            ) : (
+                <div>Nenhum produto disponível</div>
+            )}
+        </div>
+    );
 }
 
 export default ListaProdutos;
